f77893f423e6567c641952ea473b81bf2937c22f
Brandyn <bwhite@cs.umd.edu>
2010-06-11 01:16:19 -0400

    Added svm

---
+++ classification_server/classifiers/svm.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# (C) Copyright 2010 Brandyn A. White
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""SVM Classifier
+"""
+
+__author__ = 'Brandyn A. White <bwhite@cs.umd.edu>'
+__license__ = 'GPL V3'
+
+import numpy as np
+
+import libsvm.svm
+import libsvm.svmutil
+
+
+class SVM(object):
+    def __init__(self, options=None):
+        try:
+            self._param = ' '.join(['-%s %s' % x for x in options.items()])
+        except AttributeError:
+            self._param = ''
+        
+    def train(self, labels, values):
+        """Build a model.
+
+        Args:
+            labels: List of integer labels
+            values: List of numpy arrays, all with the same dimensionality
+        """
+        prob  = libsvm.svm.svm_problem(labels, values)
+        param = libsvm.svm.svm_parameter(self._param)
+        self._m = libsvm.svmutil.svm_train(prob, param)
+
+    def test(self, value):
+        """Evaluates a single value against the training data.
+
+        NOTE: Confidence is currently set to 0!
+
+        Args:
+            value: A numpy array
+
+        Returns:
+            Sorted (descending) list of (confidence, label)
+        """
+        labels, stats, confidence = libsvm.svmutil.svm_predict([-1], [value], self._m)
+        print(labels)
+        return [(0., labels[0])]
+
+def main():
+    print(__doc__)
+
+if __name__ == '__main__':
+    main()
