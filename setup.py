e6e361d959212d483b821607b8a070fdbc418f12
Brandyn White <bwhite@cs.umd.edu>
2010-08-25 04:18:53 -0400

    Changed setup to include scons call

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from distutils.core import setup
+import subprocess
 
+subprocess.check_call('scons')
 setup(name='classipy',
       version='.01',
       packages=['classipy', 'classipy.classifiers', 'classipy.classifiers.libsvm', 'classipy.classifiers.liblinear'],
