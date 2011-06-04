f5fb5b50495ac5c7284bcbc1219055a3fc21d110
Brandyn A. White <bwhite@dappervision.com>
2011-06-03 23:19:03 -0400

    Turned off profiling and added signature embedding Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- classipy/classifiers/rand_forest/rand_forest.pyx
+++ classipy/classifiers/rand_forest/rand_forest.pyx
@@ -1,4 +1,4 @@
-# cython: profile=True
+# cython: embedsignature=True
 #!/usr/bin/env python
 # (C) Copyright 2010 Brandyn A. White
 #
@@ -181,12 +181,6 @@ cpdef make_features(feature_factory, num_feat, seed=None):
 
 
 cdef class RandomForestClassifier(object):
-    """Random Forest Classifier
-
-    RandomForestClassifier(feature_factory, num_classes=2, tree_depth=4,
-        num_feat=1000, min_info=.01, trees_ser=None, verbose=True,
-        seed=None)
-    """
     cdef public object feature_factory
     cdef int tree_depth
     cdef double min_info
